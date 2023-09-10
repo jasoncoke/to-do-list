f95e904c2df71858cdf71e4790528a542aa2ea78
jasonKun <hqk616@163.com>
2023-09-10 13:17:07 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,14 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: '/list'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/list',
+    name: 'home',
+    component: () => import('@/views/list/index.vue')
   }
 ]
 
---
+++ src/views/login/set-name.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <a-modal :open="true" title="设置一个名字吧" @ok="handleOk">
+      <a-input v-model:value="name" placeholder="请输入" />
+    </a-modal>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { setName } from '@/utils/auth'
+import { message } from 'ant-design-vue'
+
+const emits = defineEmits<{(event: 'close'): void
+}>()
+
+const name = ref<string>('')
+const handleOk = () => {
+  const username:string = name.value.trim()
+  setName(username)
+  message.success('username 设置成功！')
+  emits('close')
+}
+
+</script>
+
+<style scoped>
+
+</style>
